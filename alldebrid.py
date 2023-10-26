556abe2f634c6b09b695b1295162b34fe190f7a3
MonXBZH <simon@legallou.tech>
2023-10-26 16:04:50 +0200

    Comment slug section & osrename

--- alldebrid.py
+++ alldebrid.py
@@ -175,11 +175,11 @@ for event in i.event_gen(yield_nones=False):
     if "IN_CLOSE_WRITE" in type_names:
         if filename in created_files:
             if ".torrent" in file_extension:
-                filenamefix = slugify(filename)
-                print('FIXED FILENAME', filename)
-                newfilename = filenamefix+".inprogress"
-                os.rename(filename, newfilename)
-                filename = filename+".inprogress"
+                # filenamefix = slugify(filename)
+                # print('FIXED FILENAME', filename)
+                # newfilename = filenamefix+".inprogress"
+                # os.rename(filename, newfilename)
+                # filename = filename+".inprogress"
                 print("FILENAME=[{}] EVENT_TYPES={}".format(filename, type_names))
                 print("TEST ALLDEBRID WEBSITE STATUS...")
                 url_status = check_url()
